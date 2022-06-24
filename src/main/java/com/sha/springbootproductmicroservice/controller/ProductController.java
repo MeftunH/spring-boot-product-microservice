81bcda3ff7d796df7291a1f9aa144821a8d959c2
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-24 04:11:30 +0300

    init product controller

---
+++ src/main/java/com/sha/springbootproductmicroservice/controller/ProductController.java
@@ -0,0 +1,35 @@
+package com.sha.springbootproductmicroservice.controller;
+
+import com.sha.springbootproductmicroservice.model.Product;
+import com.sha.springbootproductmicroservice.service.IProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/1.0/products")
+public class ProductController {
+    private final IProductService productService;
+
+    public ProductController(IProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> saveProduct(@RequestBody Product product) {
+        return new ResponseEntity<>(productService.saveProduct(product), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllProducts() {
+        return ResponseEntity.ok(productService.findAll());
+    }
+}
