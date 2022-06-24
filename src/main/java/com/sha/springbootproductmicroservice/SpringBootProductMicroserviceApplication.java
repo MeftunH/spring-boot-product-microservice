ae7d272b734d4c858f86995f48714aadd38114cc
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-24 03:13:57 +0300

    Initial commit

---
+++ src/main/java/com/sha/springbootproductmicroservice/SpringBootProductMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.sha.springbootproductmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProductMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootProductMicroserviceApplication.class, args);
+    }
+
+}
